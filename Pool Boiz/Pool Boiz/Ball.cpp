2a12334016b5c741bea56c0f1119ada099a0cdc3
shinigamirei <shinigamireyas@googlemail.com>
2017-04-06 13:11:10 +0100

    ball improvements
    asdasd

--- Pool Boiz/Pool Boiz/Ball.cpp
+++ Pool Boiz/Pool Boiz/Ball.cpp
@@ -9,25 +9,27 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #define PI 3.14159265
+
 Ball::Ball()
 {
 	lats = 6;
 	longs = 6;
 	radius = 3.0;
 	indicesCount = 0;
+	velocity = glm::vec3{0,0,2};
 
-/*	for (int i = 0; i < lats; i++)
+	for (int i = 0; i < lats; i++)
 	{
 
 		float V = i / (float)lats;
-		float phi = V * glm::pi <float>();// work out the angle of the lat
+		float phi = V * PI;// work out the angle of the lat
 
 										  // Loop Through Slices
 		for (int j = 0; j <= longs; ++j)
 		{
 
 			float U = j / (float)longs;
-			float theta = U * (glm::pi <float>() * 2);//angle of long
+			float theta = U * (PI * 2);//angle of long
 
 													  // Calc The Vertex Positions
 			float x = cosf(theta) * sinf(phi);
@@ -35,11 +37,11 @@ Ball::Ball()
 			float z = sinf(theta) * sinf(phi);
 
 			// Push Back Vertex Data
-			vertices.push_back(x * Radius);
-			vertices.push_back(y * Radius);
-			vertices.push_back(z * Radius);
+			vertices.push_back(x * radius);
+			vertices.push_back(y * radius);
+			vertices.push_back(z * radius);
 
-			std::cout << i << std::endl << j << std::endl;
+			std::cout << y << std::endl;
 		}
 	}
 	// Calc The Index Positions
@@ -47,10 +49,12 @@ Ball::Ball()
 
 		indices.push_back(i);
 		indices.push_back(i + longs);
-		std::cout << indices[i]<<std::endl;
+		indices.push_back(i + longs +1);
 
 		indicesCount += 2;
-	}*/
+	}
+	for (int h = 0; h < longs*lats;h++)
+		std::cout << indices[h]<<std::endl;
 }
 
 
@@ -74,6 +78,53 @@ GLuint Ball::MakeVao()
 		GLuint this_vao;
 		glGenVertexArrays(1, &this_vao);
 		glBindVertexArray(this_vao);
+		
+//		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+		std::vector<float> verticies
+		{
+		};
+		
+		verticies.push_back(-1.0);
+		verticies.push_back(0.0);
+		verticies.push_back(-1.0);
+
+		verticies.push_back(-1.0);
+		verticies.push_back(0.0);
+		verticies.push_back(1.0);
+
+		verticies.push_back(1.0);
+		verticies.push_back(0.0);
+		verticies.push_back(-1.0);
+
+		verticies.push_back(1.0);
+		verticies.push_back(0.0);
+		verticies.push_back(1.0);
+
+		verticies.push_back(1.0);
+		verticies.push_back(2.0);
+		verticies.push_back(1.0);
+
+		verticies.push_back(-1.0);
+		verticies.push_back(2.0);
+		verticies.push_back(1.0);
+
+		verticies.push_back(-1.0);
+		verticies.push_back(2.0);
+		verticies.push_back(-1.0);
+
+		verticies.push_back(1.0);
+		verticies.push_back(2.0);
+		verticies.push_back(-1.0);
+		
+		for (int g = 0; g < 24;g++)
+			std::cout << verticies[g] << std::endl;
+		std::vector<unsigned int> in
+		{
+			0, 1, 2, 3, 4, 1, 5, 0, 6, 7, 5, 4, 2, 7, 6
+		};
+
+
 		float verts[]
 		{
 			-2.0,0.0,-2.0,
@@ -96,92 +147,50 @@ GLuint Ball::MakeVao()
 			0.0,0.0,1.0,
 			0.0,0.0,1.0,
 			0.0,0.0,1.0,
+			0.0,0.0,1.0,
+			0.0,0.0,1.0,
+			0.0,0.0,1.0,
+			0.0,0.0,1.0,
+			0.0,0.0,1.0,
+			0.0,0.0,1.0,
+			0.0,0.0,1.0,
+			0.0,0.0,1.0,
 		};
-
+	
 		unsigned int indec[]{0,1,2,3,4,1,5,0,6,7,5,4,2,7,6};
 		//Vertex vbo
 		{
+			float size = verticies.size() * sizeof (float);
 			GLuint vbo;
 			glGenBuffers(1, &vbo);
 			glBindBuffer(GL_ARRAY_BUFFER, vbo);
-			glBufferData(GL_ARRAY_BUFFER,sizeof(vertices) *sizeof(float), &vertices[0], GL_STATIC_DRAW);
+			glBufferData(GL_ARRAY_BUFFER,size, &verticies[0], GL_STATIC_DRAW);
 			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
 			glEnableVertexAttribArray(0);
 		}
 		//Vertex colours
-		{
+/*		{
 			GLuint vbo;
 			glGenBuffers(1, &vbo);
 			glBindBuffer(GL_ARRAY_BUFFER, vbo);
 			glBufferData(GL_ARRAY_BUFFER, sizeof(colours) * sizeof(float), &colours[0], GL_STATIC_DRAW);
 			glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, NULL);
 			glEnableVertexAttribArray(1);
-		}
+		}*/
 		//Index vbo
 		{
+			unsigned int indicesSize = indices.size() * sizeof(unsigned int);
 			GLuint vbo;
 			glGenBuffers(1, &vbo);
 			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbo);
-			glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(vertices) * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+			glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indec), &indec[0], GL_STATIC_DRAW);
 		}
 		glBindVertexArray(0);
 		return (this_vao);
 	}
 }
 
-// Fill the vertex array with co-ordinates of the sample points.
-void Ball::fillHemVertexArray(glm::vec4 hemVertices[(HEM_LONGS + 1) * (HEM_LATS + 1)])
+void Ball::sideCollision()
 {
-	int i, j, k;
 
-	k = 0;
-	for (j = 0; j <= lats; j++)
-		for (i = 0; i <= longs; i++)
-		{
-			hemVertices[k].x = radius * cos((float)j / lats * PI / 2.0) * cos(2.0 * (float)i / longs * PI);
-			hemVertices[k].y = radius * sin((float)j / lats * PI / 2.0);
-			hemVertices[k].z = radius * cos((float)j / lats * PI / 2.0) * sin(2.0 * (float)i / longs * PI);
-			hemVertices[k].w = 1.0;
-			k++;
-		}
-}
-
-// Fill the array of index arrays.
-void Ball::fillHemIndices(unsigned int hemIndices[HEM_LATS][2 * (HEM_LONGS + 1)])
-{
-	int i, j;
-	for (j = 0; j < HEM_LATS; j++)
-	{
-		for (i = 0; i <= HEM_LONGS; i++)
-		{
-			hemIndices[j][2 * i] = (j + 1)*(HEM_LONGS + 1) + i;
-			hemIndices[j][2 * i + 1] = j*(HEM_LONGS + 1) + i;
-		}
-	}
-}
-
-// Fill the array of counts.
-void Ball::fillHemCounts(int hemCounts[HEM_LATS])
-{
-	int j;
-	for (j = 0; j < HEM_LATS; j++) hemCounts[j] = 2 * (HEM_LONGS + 1);
-}
-
-// Fill the array of buffer offsets.
-void Ball::fillHemOffsets(void* hemOffsets[HEM_LATS])
-{
-	int j;
-	for (j = 0; j < HEM_LATS; j++) hemOffsets[j] = (GLvoid*)(2 * (HEM_LONGS + 1)*j * sizeof(unsigned int));
-}
-
-// Initialize the hemisphere.
-void Ball::fillHemisphere(glm::vec4 hemVertices[(HEM_LONGS + 1) * (HEM_LATS + 1)],
-	unsigned int hemIndices[HEM_LATS][2 * (HEM_LONGS + 1)],
-	int hemCounts[HEM_LATS],
-	void* hemOffsets[HEM_LATS])
-{
-	fillHemVertexArray(hemVertices);
-	fillHemIndices(hemIndices);
-	fillHemCounts(hemCounts);
-	fillHemOffsets(hemOffsets);
 }
